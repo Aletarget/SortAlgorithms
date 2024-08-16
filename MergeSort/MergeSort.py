39a06ccca53fa98be2cf1c21c155982538b0617f
Aletarget <alejomora1205@gmail.com>
2024-08-16 16:55:07 -0500

    Añadidos comentarios para la interpretacion del funcionamiento

--- MergeSort/MergeSort.py
+++ MergeSort/MergeSort.py
@@ -4,12 +4,13 @@ def merge_sort(array):
         right_array = array[len(array)//2:]
         #Uso de recursividad para la division de los arreglos que ya se tienen.
         merge_sort(left_array)
+        
         merge_sort(right_array)
 
         i = 0 # Contador del array izquierdo
         j = 0 # Contador del array derecho
         k = 0
-        while i < len(left_array) and j < len(right_array):
+        while i < len(left_array) and j < len(right_array): #Si alguno deja de ser verdad significa que almenos uno de los arreglos ya se asigno al arreglo original
             """
             Comparar si el primer elemento izquierdo de cada array es mayor o menor, si es menor, se aumenta el contador de ese arreglo y 
             se volveria a realizar la comparacion aumentando en 1 unidad el valor de q ya que es la posicion de memoria que se va asignando
@@ -21,17 +22,20 @@ def merge_sort(array):
                 array[k] = right_array[j]
                 j += 1
             k += 1
-            
+        # En el caso de que el anterior bucle while haya terminado, procedera a comprobar si alguno de los dos arreglos tiene elementos por asignar, en este caso solo se 
+        # ejecutara un while, no los dos ya que para que se rompa el anterior ciclo, alguno de los dos arreglos tuvo que ser asignado completamente.
         while i < len(left_array):
             array[k] = left_array[i]
             i += 1
             k += 1
+
         while j < len(right_array):
             array[k] = right_array[j]
             j += 1
             k += 1
-        print(array)
-            
-array_Test = [2,4,6,1,3,10,8,5,7,9]
+        
+
+array_Test = [8,3,6,1]
 
 merge_sort(array_Test)
+print(array_Test)

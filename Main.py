1994e2c2fbd16b74b0365d63d867db9fdb893f38
Aletarget <alejomora1205@gmail.com>
2024-08-16 22:03:07 -0500

    Creacion de Main para la ejecucion de los algoritmos

--- Main.py
+++ Main.py
@@ -12,10 +12,10 @@ array = carga_Datos.load_random_numbers(datos100)
 
 #LLAMADO AL MERGE-SORT
 
-inicio = time.perf_counter_ns()
+inicio = time.perf_counter_ns() #Conteo en nanosegundos para comenzar a contar el tiempo de duracion del algoritmo
 MergeSort.merge_sort(array)
 fin = time.perf_counter_ns()
-tiempoFinal = (fin - inicio)/pow(10,6)
-print("Tiempo total para los datos: ", tiempoFinal , "ms\n")
+tiempoFinal = (fin - inicio)/pow(10,6) #Conversion de nanosegundos a milisegundos
+print("Tiempo total para ordenar los datos mediante el metodo Merge Sort: ", tiempoFinal , "ms\n")
 print(array)
 
